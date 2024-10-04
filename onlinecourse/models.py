85c9424709b4b10dff2222b6476ad650a9734950
c-u-p <umeshcpadul@gmail.com>
2024-10-04 09:30:32 -0400

    Debugged and Completed Django Project

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -112,7 +112,7 @@ class Question(models.Model):
 
 # Choice model
 class Choice(models.Model):
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
